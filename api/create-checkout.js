db52c6ebe2c07e75d8025d584f53120d090c8786
-m <-m>
2026-02-28 23:12:58 -0300

    Integração de pagamento Boleto

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   try {
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
+      payment_method_types: ["card","boleto"],
       mode: "payment",
       line_items: [
         {
